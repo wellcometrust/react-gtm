9e558c283d4ed7f1fe70d1191d865122066da556
Dipak Shrestha <d.shrestha@wellcome.org>
2024-06-07 11:51:57 +0100

    Update snippets file wit new script.

--- src/Snippets.js
+++ src/Snippets.js
@@ -12,14 +12,20 @@ const Snippets = {
     const iframe = `
       <iframe src="https://www.googletagmanager.com/ns.html?id=${id}${gtm_auth}${gtm_preview}&gtm_cookies_win=x"
         height="0" width="0" style="display:none;visibility:hidden" id="tag-manager"></iframe>`
-  
+
     const script = `
-      (function(w,d,s,l,i){w[l]=w[l]||[];
-        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js', ${JSON.stringify(events).slice(1, -1)}});
-        var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-        j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+'${gtm_auth}${gtm_preview}&gtm_cookies_win=x';
-        f.parentNode.insertBefore(j,f);
-      })(window,document,'script','${dataLayerName}','${id}');`
+      dataLayer = {
+        'gtm.start': {
+          ${id}: true,
+        }
+      };
+    </script>
+    <script src="https://www.googletagmanager.com/gtag/js"></script>
+    <script>
+      gtag('js', {
+        'gtm.start': dataLayer['gtm.start']
+      });
+    `;
   
     const dataLayerVar = this.dataLayer(dataLayer, dataLayerName)
   
